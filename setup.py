096f8bbdba4ecb04e648b568152bbbde67b97386
Esben Sonne <esbensonne@gmail.com>
2014-09-28 15:54:49 +0200

    Improved setup.py.

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ setup(
     name='beetle_s3uploader',
     author='Esben Sonne',
     author_email='esbensonne+code@gmail.com',
+    description='Beetle plugin to upload the site to S3',
     url='https://github.com/cknv/beetle-s3uploader',
     license='MIT',
     packages=[
         'beetle_s3uploader'
     ],
     install_requires=[
-        'boto==2.32.1'
+        'boto'
     ]
 )
